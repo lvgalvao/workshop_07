f1788466fa739a64801b6d9a504cb2e4245f37a3
Luciano Filho <lvgalvaofilho@gmail.com>
2023-10-07 02:54:45 -0300

    finalizei o módulo de extract

--- app/ETL/extract.py
+++ app/ETL/extract.py
@@ -1,2 +1,23 @@
-def extract_from_excel():
-    return "extract_from_excel"
+import os # biblioteca para manipular arquivos e pastas
+import glob # biblioteca para listar arquivos de uma pasta
+
+import pandas as pd
+
+
+def extract_from_excel(path_input: str) -> list[pd.DataFrame]:
+    """o objetivo dessa função é extrar os dados de diversos arquivos em excel, terá como saída uma lista de dataframes"""
+    
+    files = glob.glob(os.path.join(path_input, "*.xlsx")) # lista todos os arquivos com a extensão .xlsx
+    if not files:
+        raise FileNotFoundError("Nenhum arquivo encontrado")
+    
+    all_data = []
+    for file in files:
+        data = pd.read_excel(file)
+        all_data.append(data)
+
+    return all_data
+
+if __name__ == "__main__":
+    data = extract_from_excel(path_input="./data/input")
+    print(data)
--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from ETL.extract import extract_from_excel
 # from ETL.load import
 # from ETL.transformation import
 
-def pipeline():
-    data = extract_from_excel()
+def pipeline(path_input: str):
+    data = extract_from_excel(path_input=path_input)
     # data = transform(data)
     # load(data)
     return data
 
 if __name__ == "__main__":
-    print(pipeline())
+    print(pipeline(path_input="./data/input"))
