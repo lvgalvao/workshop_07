2b898f5cd2cd9cfbe776dbf11c7924f051a1facc
Luciano Filho <lvgalvaofilho@gmail.com>
2023-10-07 05:50:35 -0300

    refactor

--- app/main.py
+++ app/main.py
@@ -3,6 +3,17 @@ from ETL.load import load_to_excel
 from ETL.transformation import consolidate_dataframes
 
 def pipeline(path_input: str, output_path: str, file_name: str):
+    """
+    o objetivo dessa função é executar o pipeline completo
+    
+    Args:
+        path_input (str): caminho da pasta onde estão os arquivos de entrada
+        output_path (str): caminho da pasta onde será salvo o arquivo de saída
+        file_name (str): nome do arquivo de saída
+        
+    Returns:
+        str: mensagem de sucesso
+    """
     lista_de_data_frames = extract_from_excel(path_input=path_input)
     data_frame_consolidado = consolidate_dataframes(dataframes=lista_de_data_frames)
     load_to_excel(data_frame_consolidado, output_path, file_name)
---
+++ docs/index.md
@@ -0,0 +1,18 @@
+# Welcome to MkDocs
+
+For full documentation visit [mkdocs.org](https://www.mkdocs.org).
+
+## Commands
+
+```mermaid
+flowchart LR
+    subgraph ETL[Pipeline]
+        A(Multiplos Arquivos Excel) --> B[Extract: extract_from_excel] 
+        B[Extract: extract_from_excel] --> |Gera uma lista de Dataframes| C[Transformation: consolidate_dataframes]
+        C[Transformation: consolidate_dataframes] -->|Gera um Dataframe Consolidado| D[Load: Converte para Excel]
+        D(Load: Converte para Excel) --> |Salva o consolidado em Excel| E(Pasta Output: Um arquivo único Excel)
+   
+    end
+```
+
+### ::: app.main.pipeline
